9311e734d0daedb0daa17026be9aba11b0705136
yoshida0129 <alpacagame0129@gmail.com>
2020-03-17 16:09:38 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const length = 40;
+const saveFibList = [0, 1];
+
+for (let i = 0; i <= length; i++) {
+  if (i >= 2) saveFibList.push(saveFibList[i - 1] + saveFibList[i - 2])
+  console.log(saveFibList[i])
+}
